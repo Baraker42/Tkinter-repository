beca51ff1b2087120a684cfe372341ba22cf1cd4
Baraker42 <70022095+Baraker42@users.noreply.github.com>
2021-01-30 15:19:57 +0100

    Add files via upload
    add cooking function, subtracks ingrediences from storage

--- cook_book/cooking_def.py
+++ cook_book/cooking_def.py
@@ -19,24 +19,66 @@ def cook_show():
         #pravděpodobně jsem to vědel a zapomněl, protože jsem s tím už dlouho nepracoval
         recipe=recipes[i]
         print(ingrediences)
+        not_enough=0
+        miss=[]
+        miss_mass=[]
         for i in recipe:
             want_ingredience=i["surovina"]
             want_mass=i["množství"]
             #s jednotkami bude asi problém, zatím bych je rád zachoval, protože mi přijdou celkem důležité, minimálně vizuálně
             want_unit=i["jednotka"]
 
-            #vyhledá surovinu z receptu v ingrediencích a následně odečte potřebné množství - zatím neumí pracovat s tím,
-            #když se dostane do mínusu, ale to se postupně doplní
+            #vyhledá surovinu z receptu v ingrediencích a následně odečte potřebné množství  a v případě, že by se
+            #dostala po odečtení do mínusu, tak surovinu neodečte a uživatele upozorní
+            
             for search in ingrediences:
                 if search["surovina"]==want_ingredience:
                     new_mass=int(search["množství"])-int(want_mass)
-                    change={"množství":new_mass}
-                    search.update(change)
+                    if new_mass <= 0:
+                        miss.append(search["surovina"])
+                        miss_mass_var=(str(new_mass*-1)+" " + (search["jednotka"]))
+                        miss_mass.append(miss_mass_var)
 
-        #uloží změny v dostupných ingrediencích        
-        with open ("././/suroviny.json", "w") as file:
-                json.dump(ingrediences, file)      
+                        not_enough=+1
+                            
+                        #nové rozhraní
+                        not_enough_popup=Tk()
+                        not_enough_popup.title("Receptář")
+                        not_enough_popup.iconbitmap("cook.ico")
+
+                        #štítky
+                        not_enough_label=Label(not_enough_popup,text="Nemáš dostatek následujících surovin")
+                        not_enough_label.grid(row=0,column=0,columnspan=2)
+                        counter=1
+                        for missing in miss:
+                            missing_mass=miss_mass[counter-1]
+
+                            missing_label=Label(not_enough_popup, text=missing)
+                            missing_mass_label=Label(not_enough_popup, text=missing_mass)
+
+                            missing_label.grid(row=counter,column=0)
+                            missing_mass_label.grid(row=counter,column=1)
+
+                            counter+=1
+
+
+
+
+
+    
+                    if new_mass >= 0:
+                        change={"množství":new_mass}
+                        search.update(change)
         
+        #uloží změny v dostupných ingrediencích
+        
+        if not_enough == 0:        
+            with open ("././/suroviny.json", "w") as file:
+                    json.dump(ingrediences, file)
+                    
+            
+        if not_enough >=0:  
+            pass
             
 
         
